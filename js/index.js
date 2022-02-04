15e4941637a949bb5838e2b40d6c2abe1c54137c
Le Luu <lemanluu@netspace.net.au>
2022-02-04 15:15:09 +1100

    Self Assessment

--- js/index.js
+++ js/index.js
@@ -1,30 +1,10 @@
-/* ***************************
-  JWD JavaScript Assessment
-
-  This code is unfinished. You will need to study it to figure out what it does. Then you will need to use this and
-  your own code, to finish the app. 
-  
-  The tasks you need to do are below.
-
-    TASKS TODO:
-      1. Calculate the score as the total of the number of correct answers
-
-      2. Add an Event listener for the submit button, which will display the score and highlight 
-         the correct answers when the button is clicked. Use the code from lines 67 to 86 to help you.
-
-      3. Add 2 more questions to the app (each question must have 4 options).
-
-      4. Reload the page when the reset button is clicked (hint: search window.location)
-
-      5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
-*************************** */
-
 window.addEventListener('DOMContentLoaded', () => {
-  const start = document.querySelector('#start');
+  const start = document.querySelector('#start')
   start.addEventListener('click', function (e) {
-    document.querySelector('#quizBlock').style.display = 'block';
-    start.style.display = 'none';
-  });
+    document.querySelector('#quizBlock').style.display = 'block'
+    start.style.display = 'none'
+  })
+
   // quizArray QUESTIONS & ANSWERS
   // q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
   // Basic ideas from https://code-boxx.com/simple-javascript-quiz/
@@ -44,12 +24,22 @@ window.addEventListener('DOMContentLoaded', () => {
       o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
       a: 1,
     },
-  ];
+    {
+      q: 'What is the name of the 12th month in a year?',
+      o: ['December', 'March', 'May', 'November'],
+      a: 0,
+    },
+    {
+      q: 'Who is the priminister of Australia in 2022',
+      o: ['Kelvin Rud', 'Julia Guilard', 'Scott Morrison', 'John Howard'],
+      a: 2,
+    },
+  ]
 
   // function to Display the quiz questions and answers from the object
   const displayQuiz = () => {
-    const quizWrap = document.querySelector('#quizWrap');
-    let quizDisplay = '';
+    const quizWrap = document.querySelector('#quizWrap')
+    let quizDisplay = ''
     quizArray.map((quizItem, index) => {
       quizDisplay += `<ul class="list-group">
                    Q - ${quizItem.q}
@@ -58,33 +48,78 @@ window.addEventListener('DOMContentLoaded', () => {
                     <li class="list-group-item"  id="li_${index}_2"><input type="radio" name="radio${index}" id="radio_${index}_2"> ${quizItem.o[2]}</li>
                     <li class="list-group-item"  id="li_${index}_3"><input type="radio" name="radio${index}" id="radio_${index}_3"> ${quizItem.o[3]}</li>
                     </ul>
-                    <div>&nbsp;</div>`;
-      quizWrap.innerHTML = quizDisplay;
-    });
-  };
+                    <div>&nbsp;</div>`
+      quizWrap.innerHTML = quizDisplay
+    })
+  }
 
   // Calculate the score
   const calculateScore = () => {
-    let score = 0;
+    let score = 0
     quizArray.map((quizItem, index) => {
       for (let i = 0; i < 4; i++) {
         //highlight the li if it is the correct answer
-        let li = `li_${index}_${i}`;
-        let r = `radio_${index}_${i}`;
-        liElement = document.querySelector('#' + li);
-        radioElement = document.querySelector('#' + r);
+        let li = `li_${index}_${i}`
+        let r = `radio_${index}_${i}`
+        liElement = document.querySelector('#' + li)
+        radioElement = document.querySelector('#' + r)
 
         if (quizItem.a == i) {
           //change background color of li element here
+          liElement.style.background = 'pink'
         }
 
         if (radioElement.checked) {
           // code for task 1 goes here
+          if (quizItem.a == i) {
+            score += 1
+          }
         }
       }
-    });
-  };
+      document.querySelector('#score').innerHTML = 'YOU SCORED: ' + score
+    })
+  }
 
   // call the displayQuiz function
-  displayQuiz();
-});
+  displayQuiz()
+
+  // Add a submit button to calculate score
+  btnSubmit.addEventListener('click', calculateScore)
+
+  //Add a reset button
+  btnReset.addEventListener('click', refreshed)
+  function refreshed() {
+    displayQuiz()
+    window.location.href = './index.html'
+  }
+
+  //Add a countdown timer
+  document.getElementById('time').innerHTML = 01 + ':' + 02
+  startTimer()
+  function startTimer() {
+    var presentTime = document.getElementById('time').innerHTML
+    var timeArray = presentTime.split(/[:]+/)
+    var minutes = timeArray[0]
+    var seconds = checkSecond(timeArray[1] - 1)
+    if (seconds == 59) {
+      minutes = minutes - 1
+    }
+    if (minutes < 0) {
+      btnSubmit.click()
+      return
+    }
+    document.getElementById('time').innerHTML = minutes + ':' + seconds
+    console.log(minutes)
+    setTimeout(startTimer, 1000)
+  }
+  function checkSecond(sec) {
+    if (sec < 10 && sec >= 0) {
+      sec = '0' + sec
+    }
+    if (sec < 0) {
+      sec = '59'
+    }
+    return sec
+  }
+})
+
